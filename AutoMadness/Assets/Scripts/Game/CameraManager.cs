c80db4275dedab10e5b05930d1043ea5fb4b5726
GuilhermeRameh <guilhermedr@al.insper.edu.br>
2023-04-14 15:42:27 -0300

    avancos individuais

--- AutoMadness/Assets/Scripts/Game/CameraManager.cs
+++ AutoMadness/Assets/Scripts/Game/CameraManager.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    public static CameraManager instance;
+    void Awake() { instance = this; }
+
     public GameObject player;
     private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
+
         offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        if (wkManager.InWindow()==false)
+        {
+            GetComponent<Camera>().orthographicSize = 5f;
+            transform.position = player.transform.position + offset;
+        }
+        else
+        {
+            transform.position = new Vector3(15, 6, -10);
+            GetComponent<Camera>().orthographicSize = 12f;
+        }
     }
 }
--- AutoMadness/Assets/Scripts/Game/wkManager.cs
+++ AutoMadness/Assets/Scripts/Game/wkManager.cs
@@ -4,36 +4,71 @@ using UnityEngine;
 
 public class wkManager : MonoBehaviour
 {
+    public wkManager instance;
+    void Awake() { instance = this; }
+
     public List<Collider2D> workStations;
     private int wsId = -1;
+    public static bool in_window = false;
 
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> WSMenus;
+
+    public void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        if(collider.tag=="WK")
+        {
+            wsId = int.Parse(collider.name.Split("WS")[1])-1;
+            // Debug.Log(wsId);
+        }
+        if(collider.tag=="window_collider")
+        {
+            in_window = true;
+        }
     }
 
-    // public void OnTriggerEnter2D(Collider2D collider)
-    // {
-    //     if(collider.tag=="WK")
-    //     {
-    //         Debug.Log("Colidiu");
-    //         collider.OnCollisionStay();
-    //     }
-    // }
-
-    void OnCollisionEnter2D(Collision2D collision)
+    public void OnTriggerExit2D(Collider2D collider)
     {
-        Debug.Log("Colision:");
-        if (collision.gameObject.tag == "WK")
+        if (collider.tag=="WK")
         {
-            Debug.Log("AAAAAAAAAAAA");
+            WSMenus[wsId].SetActive(false);
+            wsId = -1;
+        }
+        else
+        {
+            in_window = false;
+        }
+    }
+
+    void OldInputSystem()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (wsId != -1)
+            {
+                Popup();
+                Debug.LogFormat("Abre o Menu {0}", wsId);
+            } 
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OldInputSystem();
     }
+
+    void Popup()
+    {
+        WSMenus[wsId].SetActive(true);
+    }
+    public void ClosePopup()
+    {
+        WSMenus[wsId].SetActive(false);
+    }
+
+    public static bool InWindow()
+    {
+        return in_window;
+    }
+
 }
