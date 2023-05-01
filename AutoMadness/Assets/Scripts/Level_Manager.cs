b18ece4b85410ca2a9ceeb15b2b34f648514785d
acsouza2398 <63055644+acsouza2398@users.noreply.github.com>
2023-04-30 21:30:10 -0300

    Level 4, level 5 e tank

--- AutoMadness/Assets/Scripts/Level_Manager.cs
+++ AutoMadness/Assets/Scripts/Level_Manager.cs
@@ -13,12 +13,18 @@ public class Level_Manager : MonoBehaviour
     public float enemy_ranged_dmg;
     public float enemy_melee_speed;
     public float enemy_ranged_speed;
+    public float tank_health;
+    public float tank_dmg;
+    public float tank_speed;
+    public float tank_shoot_speed;
+    public float goal_health;
 
     public float unit_shoot_speed;
     public float unit_melee_dmg;
     public float unit_ranged_dmg;
     public float unit_melee_speed;
     public float unit_ranged_speed;
+    public float spawn_health;
     public int wallet;
     public int level;
     // Start is called before the first frame update
@@ -39,6 +45,8 @@ public class Level_Manager : MonoBehaviour
             unit_melee_speed = 5;
             unit_ranged_speed = 3;
             enemy_health = 2;
+            goal_health = 10;
+            spawn_health = 10;
             wallet = 5;
             level = 1;
 
@@ -55,6 +63,8 @@ public class Level_Manager : MonoBehaviour
             unit_melee_speed = 5;
             unit_ranged_speed = 3;
             enemy_health = 3;
+            goal_health = 10;
+            spawn_health = 10;
             wallet = 10;
             level = 2;
         } else if (currentScene.name == "Level3"){
@@ -70,8 +80,48 @@ public class Level_Manager : MonoBehaviour
             unit_melee_speed = 5;
             unit_ranged_speed = 3;
             enemy_health = 3;
-            wallet = 10;
+            goal_health = 15;
+            spawn_health = 15;
+            wallet = 15;
             level = 3;
+        } else if (currentScene.name == "Level4"){
+            enemy_spawn_speed = 5;
+            enemy_shoot_speed = 2;
+            enemy_melee_dmg = 1.5f;
+            enemy_ranged_dmg = 1;
+            enemy_melee_speed = 4.5f;
+            enemy_ranged_speed = 2.5f;
+            unit_shoot_speed = 2;
+            unit_melee_dmg = 1.5f;
+            unit_ranged_dmg = 1;
+            unit_melee_speed = 5;
+            unit_ranged_speed = 3;
+            enemy_health = 3;
+            goal_health = 15;
+            spawn_health = 15;
+            wallet = 15;
+            level = 4;   
+        }   else if (currentScene.name == "Level5"){
+            enemy_spawn_speed = 4;
+            enemy_shoot_speed = 2;
+            enemy_melee_dmg = 1.5f;
+            enemy_ranged_dmg = 1;
+            enemy_melee_speed = 4.5f;
+            enemy_ranged_speed = 2.5f;
+            unit_shoot_speed = 2;
+            unit_melee_dmg = 1.5f;
+            unit_ranged_dmg = 1;
+            unit_melee_speed = 5;
+            unit_ranged_speed = 3;
+            enemy_health = 3;
+            goal_health = 20;
+            spawn_health = 20;
+            wallet = 20;
+            level = 5;
+            tank_health = 10;
+            tank_dmg = 2;
+            tank_speed = 2;
+            tank_shoot_speed = .5f;
         }
     }
 
